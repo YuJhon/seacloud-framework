b99cf8ca94f26d0b954f982cb7d907a672493a68
YuJhon <3291371805@qq.com>
2018-07-29 13:26:30 +0800

    测试CI

---
+++ seacloud-commons/src/main/java/com/sea/cloud/app/AppEnv.java
@@ -0,0 +1,19 @@
+package com.sea.cloud.app;
+
+import java.util.Properties;
+
+/**
+ * <p>功能描述</br></p>
+ *
+ * @author jiangy19
+ * @version v1.0
+ * @projectName seacloud-modules
+ * @date 2018/7/29 13:24
+ */
+public class AppEnv {
+
+  private String appId;
+  private String appSecret;
+  private AppVo appVo;
+  private Properties properties;
+}
---
+++ seacloud-commons/src/main/java/com/sea/cloud/app/AppVo.java
@@ -0,0 +1,16 @@
+package com.sea.cloud.app;
+
+/**
+ * <p>功能描述</br></p>
+ *
+ * @author jiangy19
+ * @version v1.0
+ * @projectName seacloud-modules
+ * @date 2018/7/29 13:25
+ */
+public class AppVo {
+
+  private String appId;
+
+  private String appSecret;
+}
